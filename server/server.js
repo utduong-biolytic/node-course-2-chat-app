da68844d5b4fe90c04a67cffd2414f738789a669
BIOLYTIC\ut.duong <ut.duong@biolytic.com>
2017-05-16 09:33:43 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,15 +12,36 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-
+    console.log('New user connected')
+    // socket emit from admin text Welcome to the Chat app
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text:'Welcome to the Chat App',
+        createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit from Admin text new user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    });
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+
+        
+
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
     });
 
     socket.on('disconnect', () => {
